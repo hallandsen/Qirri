95c248da19fe21563d274286e1e5d88382c76027
hallandsen <jacob.e.halland@gmail.com>
2016-06-16 13:30:13 +0200

    initial layout

---
+++ css/profile.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  $('.close').click(function(){
+    // $(this).addClass('close-slide_in');
+    console.log('fire');
+    $(this).addClass('hide');
+  });
+});
---
+++ js/profile.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+
+//   $('body').click(function(e) {
+//     if ($(e.target).closest('.close-slide_in').length === 0) {
+//       console.log('fire');
+//       $('.close-slide').removeClass('close-slide_in');
+//     }
+//   });
+
+//   $('.close').click(function(){
+//     $(this).parent('.users-area').find('.close-slide').addClass('close-slide_in');
+//   });
+
+
+
+// });
